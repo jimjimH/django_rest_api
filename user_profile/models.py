96589c3f7a252ca5ee917d687e691167674851b3
jimjimH <hi64852@hotmail.com>
2020-07-16 01:12:14 +0800

    add a module 'user_profile'

---
+++ user_profile/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField()
+    gender = models.CharField(max_length=10, blank=True, choices=GENDER_CHOICES)
+    phone = models.CharField(max_length=30, blank=True)
