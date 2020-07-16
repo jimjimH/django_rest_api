00fef78b9d42c17f61131527298b65c73e1fa92c
jimjimH <hi64852@hotmail.com>
2020-07-16 15:11:01 +0800

    try djangorestframework

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your views here.
+'''
+from django.http import JsonResponse
+from django.core import serializers
+
+
+def index(request):
+    users = User.objects.all().select_related('profile')
+    print(list(users.values()))
+    # print(users[0].profile.gender)
+    # return render(request, 'index.html', locals())
+
+    return JsonResponse({'result': list(users.values())})
+
+    user_json = serializers.serialize('json', users)
+    return HttpResponse(user_json, content_type='application/json')
+'''
+
+from rest_framework import viewsets
+from .serializers import ProfileSerializer
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
